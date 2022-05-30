f8adaacdd8f7276a2fe055727f16144c642faee9
weihuoqiang <3040200185@qq.com>
2022-05-30 13:16:57 +0800

    fix

---
+++ app.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+from flask import Flask
+from redis import Redis
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379) # ''中的redis对应服务名
+@app.route('/')
+def hello():
+	count = redis.incr('hits')
+	return 'Hello World! I have been seen {} times.\n'.format(count)
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True)
